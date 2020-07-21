c8c99d3470f463ec1e394dd122ca0c62e6bbd48b
Joacim Magnusson <joacim@isogram.se>
2020-07-21 23:06:27 +0200

    feat: added procedurally generated scrolling terrain

---
+++ src/Terrain.ts
@@ -0,0 +1,113 @@
+import * as THREE from "three";
+
+class Terrain {
+  private geometry: THREE.BufferGeometry;
+  private mesh: THREE.Mesh;
+  private dimensions = {
+    width: 200,
+    height: 200,
+    widthSegments: 30,
+    heightSegments: 30,
+  };
+
+  public object: THREE.Object3D;
+
+  constructor() {
+    this.object = new THREE.Object3D();
+    this.geometry = new THREE.PlaneBufferGeometry(
+      this.dimensions.width,
+      this.dimensions.height,
+      this.dimensions.widthSegments,
+      this.dimensions.heightSegments
+    );
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x666666,
+      flatShading: true,
+    });
+
+    console.log("constructing terrain");
+
+    this.randomize();
+
+    this.mesh = new THREE.Mesh(this.geometry, material);
+    this.mesh.rotateX(THREE.MathUtils.degToRad(-90));
+    this.mesh.position.z -= 50;
+
+    this.object.add(this.mesh);
+  }
+
+  public animate() {
+    //this.mesh.position.z += 0.1;
+    const newPositions = Array.from(this.geometry.attributes.position.array);
+
+    let x = 0,
+      y = 0,
+      z = 0,
+      index = 0;
+
+    let clippingPoint =
+      -1 *
+      (this.dimensions.height / 2 +
+        this.dimensions.height / this.dimensions.heightSegments);
+
+    for (let i = 0, l = newPositions.length / 3; i < l; i++) {
+      x = index++;
+      y = index++;
+      z = index++;
+
+      newPositions[y] -= 0.5;
+
+      if (newPositions[y] < clippingPoint) {
+        let xpos = newPositions[x];
+        let ypos =
+          newPositions[y] +
+          this.dimensions.height +
+          this.dimensions.height / this.dimensions.heightSegments;
+        let zpos = Math.random() * Math.abs(newPositions[x] / 4);
+
+        // We're flipping x and z below, in order to keep the correct
+        // order after adding to the beginning of the array.
+        newPositions.splice(x, 1);
+        newPositions.unshift(zpos);
+
+        newPositions.splice(y, 1);
+        newPositions.unshift(ypos);
+
+        newPositions.splice(z, 1);
+        newPositions.unshift(xpos * -1);
+      }
+    }
+
+    this.geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(newPositions, 3)
+    );
+
+    this.geometry.computeVertexNormals();
+    this.geometry.computeBoundingBox();
+  }
+
+  public randomize() {
+    const newPositions = Array.from(this.geometry.attributes.position.array);
+
+    let x = 0,
+      y = 0,
+      z = 0,
+      index = 0;
+
+    for (let i = 0, l = newPositions.length / 3; i < l; i++) {
+      x = index++;
+      y = index++;
+      z = index++;
+
+      newPositions[z] = Math.random() * Math.abs(newPositions[x] / 4);
+    }
+
+    this.geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(newPositions, 3)
+    );
+  }
+}
+
+export { Terrain };
--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -1,35 +1,53 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as THREE from "three";
 import { SpinningCube } from "./SpinningCube";
+import { Terrain } from "./Terrain";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-var animateId: number = -1;
+let animateId: number = -1;
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+const lineBox = new SpinningCube();
+const terrain = new Terrain();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const ThreeScene = () => {
-  const [scene] = useState(new THREE.Scene());
-  const [camera] = useState(
-    new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-  );
-  const [renderer] = useState(new THREE.WebGLRenderer());
-  const [lineBox] = useState(new SpinningCube());
   const [isAnimating, setIsAnimating] = useState(true);
   const renderEl = useRef<HTMLDivElement>(document.createElement("div"));
 
   useEffect(() => {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    scene.add(lineBox.object);
+    renderer.setPixelRatio(window.devicePixelRatio);
     camera.position.z = 50;
 
+    scene.add(lineBox.object);
+    scene.add(terrain.object);
+
+    const light = new THREE.DirectionalLight(0xffffff, 0.5);
+    light.position.set(0, 20, 0);
+    scene.add(light);
+
+    terrain.object.position.y += -10;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxPolarAngle = Math.PI * 0.5;
+    controls.minDistance = 10;
+    controls.maxDistance = 5000;
+
     renderEl.current.appendChild(renderer.domElement);
-  }, [camera, scene, lineBox, renderer]);
+  }, []);
 
   useEffect(() => {
     const animate = () => {
-      isAnimating && lineBox.animate();
+      if (isAnimating) {
+        lineBox.animate();
+        terrain.animate();
+      }
 
       renderer.render(scene, camera);
       animateId = requestAnimationFrame(animate);
@@ -37,7 +55,7 @@ const ThreeScene = () => {
 
     cancelAnimationFrame(animateId);
     animate();
-  }, [isAnimating, lineBox, renderer, scene, camera]);
+  }, [isAnimating]);
 
   return (
     <div>
