9fec14144081a7dcdcc4ec210fb7c756fd8bf9a4
Joacim Magnusson <joacim@isogram.se>
2020-07-24 13:09:27 +0200

    feat: add window resize handler

--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -73,6 +73,16 @@ const ThreeScene = () => {
     });
 
     renderEl.current.appendChild(renderer.domElement);
+
+    const handleResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
