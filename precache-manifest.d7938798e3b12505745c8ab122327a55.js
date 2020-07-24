8c21c104d7a00881d236a32aeb9abeaccedec317
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 13:05:38 +0000

    Deploy splashdust/isogram_se to github.com/splashdust/isogram_se.git:gh-pages

---
+++ precache-manifest.d7938798e3b12505745c8ab122327a55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a4ee69c7f8a2504c36653b3a4b96eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1d89e6289928e819fb",
+    "url": "/static/css/main.36951f9f.chunk.css"
+  },
+  {
+    "revision": "463afdd06049fd8f0a83",
+    "url": "/static/js/2.6e93a11c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e93a11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1d89e6289928e819fb",
+    "url": "/static/js/main.15c2870f.chunk.js"
+  },
+  {
+    "revision": "088ce47c89315466dac3",
+    "url": "/static/js/runtime-main.cc0c892a.js"
+  },
+  {
+    "revision": "2f7ae0da23688521e74784b88ffeed87",
+    "url": "/static/media/isogram-logo-1-color-white.2f7ae0da.png"
+  },
+  {
+    "revision": "afb66bc6bac600d20fbdb6d7ffe78457",
+    "url": "/static/media/star.afb66bc6.png"
+  }
+]);
